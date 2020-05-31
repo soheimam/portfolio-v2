747bc9462f9b15f7bbdbb1c297a5addd7e1e75a7
soheima <soheima@sociossports.nl>
2020-05-31 22:25:23 +0200

    initial project page

---
+++ gatsby-config.js
@@ -0,0 +1,22 @@
+/**
+ * Configure your Gatsby site with this file.
+ *
+ * See: https://www.gatsbyjs.org/docs/gatsby-config/
+ */
+
+ 
+
+module.exports = {
+  /* Your site config here */
+  plugins: [
+    {
+      resolve: `gatsby-plugin-postcss`,
+      options: {
+        postCssPlugins: [require(`postcss-font-magician`)({ 
+          hosted: ['./src/font'] 
+        })],
+      },
+    },
+  ],
+  plugins: [],
+}
---
+++ src/pages/index.js
@@ -0,0 +1,9 @@
+import React from "react"
+import Project from '../../public/Components/Project/Project'
+
+import './reset.css'
+import './index.css'
+
+export default function Home() {
+  return <Project />
+}
